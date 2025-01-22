09bdfbe27b96d791407c4de87c55c02e48167531
gleyson003 <lop23031@byui.edu>
2025-01-22 20:54:50 -0300

    Solve bugs.

--- public/js/script.js
+++ public/js/script.js
@@ -53,31 +53,32 @@ reviewsList = [
 
 //Added reviews in the DMC Delorean Reviews
 const reviews = document.getElementById('reviews');
-
-reviewsList.forEach(function(item) {
-    const li = document.createElement('li');
-    li.textContent = `"${item.review}" (${item.note})`;
-    reviews.appendChild(li);
-});
+if (reviews) {
+    reviewsList.forEach(function (item) {
+        const li = document.createElement('li');
+        li.textContent = `"${item.review}" (${item.note})`;
+        reviews.appendChild(li);
+    });
+}
 
 //Added upgrade in the Delorean upgrades
 const upgradesContainer = document.getElementById('upgrades');
+if (upgradesContainer) {
+    upgradesList.forEach(function (item) {
+        const upgradeDiv = document.createElement('div');
+        const upgradeImg = document.createElement('img');
+        upgradeImg.src = item.imgUrl;
 
-upgradesList.forEach(function(item) {
-    const upgradeDiv = document.createElement('div');
-    const upgradeImg = document.createElement('img');
-    upgradeImg.src = item.imgUrl;
+        const upgradeName = document.createElement('p');
+        const upgradeLink = document.createElement('a');
+        upgradeLink.href = item.linkUrl;
+        upgradeLink.textContent = item.name;
+        upgradeLink.target = '_blank';
 
-    const upgradeName = document.createElement('p');
-    
-    const upgradeLink = document.createElement('a');
-    upgradeLink.href = item.linkUrl; // URL do link
-    upgradeLink.textContent = item.name;
-    upgradeLink.target = '_blank'; // Abre o link em uma nova aba (opcional)
+        upgradeName.appendChild(upgradeLink);
 
-    upgradeName.appendChild(upgradeLink);
-
-    upgradeDiv.appendChild(upgradeImg);
-    upgradeDiv.appendChild(upgradeName);
-    upgradesContainer.appendChild(upgradeDiv);
-});
+        upgradeDiv.appendChild(upgradeImg);
+        upgradeDiv.appendChild(upgradeName);
+        upgradesContainer.appendChild(upgradeDiv);
+    });
+}

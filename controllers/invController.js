b47da7d770c60ae819634d941d2c0d43f47454bc
gleyson003 <lop23031@byui.edu>
2025-02-12 21:39:15 -0300

    Solve bug in the inventory api return.

--- controllers/invController.js
+++ controllers/invController.js
@@ -184,7 +184,7 @@ invCont.updateInventory  = async (req, res, next) => {
 invCont.getInventoryJSON = async (req, res, next) => {
   const classification_id = parseInt(req.params.classification_id)
   const invData = await invModel.getInventoryByClassificationId(classification_id)
-  if (invData[0].inv_id) {
+  if (Array.isArray(invData) && invData.length > 0) {
     return res.json(invData)
   } else {
     next(new Error("No data returned"))
